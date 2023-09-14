34f65e02cb9610fe96d36146e941d6ac161fe843
mlambacollins <mlambacollins@gmail.com>
2023-09-14 06:49:55 -0700

    Commenting out code

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,10 @@ int main(void)
 
     i = 0;
 
-    /* while (i < 10) Code that is casuing infinite loop
-    {
-        putchar(i);
-    }*/
+    /*while (i < 10)*/
+    /*{*/
+        /*putchar(i);*/
+    /*}*/
 
     printf("Infinite loop avoided! \\o/\n");
 
