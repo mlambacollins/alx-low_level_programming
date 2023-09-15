1a96adec184eab43414ce956c92d3d3d7ab381c0
mlambacollins <mlambacollins@gmail.com>
2023-09-15 04:04:08 -0700

    more functions and nested loops

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle in the terminal
+ * @size: The size of the triangle
+ */
+void print_triangle(int size)
+{
+    if (size <= 0)
+    {
+        _putchar('\n');
+    }
+    else
+    {
+        int i, j;
+
+        for (i = 1; i <= size; i++)
+        {
+            for (j = 1; j <= i; j++)
+            {
+                _putchar('#');
+            }
+            _putchar('\n');
+        }
+    }
+}
