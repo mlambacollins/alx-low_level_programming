ddd15114133fa2dfd4f44e0002c3ba0bbc84c8e9
mlambacollins <mlambacollins@gmail.com>
2023-09-27 08:18:10 -0700

    Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,14 @@
+#include <stddef.h>
+#include "main.h"
+
+char *_memset(char *s, char b, unsigned int n) {
+    char *ptr = s;
+    
+    while (n > 0) {
+        *ptr = b;
+        ptr++;
+        n--;
+    }
+    
+    return s;
+}
