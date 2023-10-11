3b322322eb54677bca9d405850259d09d10643ea
mlambacollins <mlambacollins@gmail.com>
2023-10-11 00:39:51 -0700

    Defines a macro named SIZE as an abbreviation for a token

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -2,8 +2,10 @@
 #define MAIN_H
 
 /**
- * SIZE - A macro that defines the size for a specific purpose
+ * SIZE - A macro that defines the size for a specific purpose.
+ *
+ * This macro is used to define the size of something important in the program.
  */
-#define SIZE 1024 
+#define SIZE 1024
 
-#endif /* MAIN_H */ 
+#endif /* MAIN_H */
