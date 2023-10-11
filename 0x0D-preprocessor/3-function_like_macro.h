bb699196fbac1efa3e8068764e6c316fcea6a0de
mlambacollins <mlambacollins@gmail.com>
2023-10-11 00:26:23 -0700

    Computes the absolute value of a number

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,1 +1,12 @@
+#ifndef ABS_X
+#define ABS_X
+
+/**
+ * ABS - Macro to compute the absolute value of a number
+ * @x: The number
+ *
+ * Return: The absolute value of x
+ */
 #define ABS(x) ((x < 0) ? -(x) : (x))
+
+#endif /* ABS_X */
