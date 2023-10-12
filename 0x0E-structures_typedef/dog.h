ffd39692b57d9b42985b80af933c372132a5cdd7
mlambacollins <mlambacollins@gmail.com>
2023-10-11 23:22:05 -0700

    Defines a new structure representing a dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A structure representing a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Description: This structure defines a dog's characteristics.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
