dd5eae2df652258750004ccdb2f8f1a9ed0a1a35
mlambacollins <mlambacollins@gmail.com>
2023-10-13 03:33:22 -0700

    Prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,13 +3,12 @@
 
 /**
  * print_name - Print a name using a pointer to a function
- * @name: string to add
- * @f: pointer to function
+ * @name: string to print
+ * @f: pointer to function that prints the name
  *
  * Return: nothing
  */
-void print_name(char *name, void(*f)(char*))
-{
+void print_name(char *name, void(*f)(char*)) {
 	if (name == NULL || f == NULL)
 		return;
 
