e32b177efde75f89c23109d24196dc5903033830
mlambacollins <mlambacollins@gmail.com>
2023-11-09 12:04:54 +0300

    Header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
@@ -13,5 +16,15 @@ int append_text_to_file(const char *filename, char *text_content);
 void error_98(int f0, char *buffer, char *argv);
 void error_99(int f0, char *buffer, char *argv);
 void error_100(int f0, char *buffer);
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
 
 #endif /*MAIN_H*/
